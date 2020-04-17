e66191f259bbc91c544086aec786a23f6e585538
kightsonsanom <tomasz.olichwer@droidsonroids.pl>
2020-04-17 20:43:58 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:allthenews/pages/news_list/news_list_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(NewsListPage());
---
+++ lib/pages/news_list/news_list_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewsListPage extends StatefulWidget {
+  @override
+  _NewsPageState createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
