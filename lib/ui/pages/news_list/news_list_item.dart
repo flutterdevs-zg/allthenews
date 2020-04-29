ede77e731344171df81ad51f473e750c405dd4f8
Marek <maarekvogt@gmail.com>
2020-04-29 20:13:53 +0200

    [NEWS-3] added news list section

--- lib/ui/pages/news_list/news_list_item.dart
+++ lib/ui/pages/news_list/news_list_item.dart
@@ -12,6 +12,7 @@ class NewsListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _buildImage(),
         SizedBox(width: 20),
