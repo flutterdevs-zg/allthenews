d2874708f730df0344e8d39a0971eeaa0837015e
kightsonsanom <tomasz.olichwer@droidsonroids.pl>
2020-05-03 13:02:13 +0200

    !fixup PR comments

--- lib/ui/pages/news_list/primary_news_list_item.dart
+++ lib/ui/pages/news_list/primary_news_list_item.dart
@@ -1,12 +1,13 @@
 import 'package:allthenews/ui/pages/news_list/primary_news_list_entity.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 abstract class _Constants {
-  static const double newsImageRadius = 15;
-  static const double newsImageLayerOpacity = 0.7;
-  static const double authorImageRadius = 15;
-  static const double authorRightPadding = 8;
-  static const double stackItemPadding = 20;
+  static const imageRadius = 15.0;
+  static const newsImageLayerOpacity = 0.7;
+  static const authorDataSpacing = 10.0;
+  static const stackItemPadding = 20.0;
+  static const wrappingThresholdPercent = 0.65;
 }
 
 class PrimaryNewsListItem extends StatelessWidget {
@@ -17,96 +18,108 @@ class PrimaryNewsListItem extends StatelessWidget {
         super(key: key);
 
   @override
-  Widget build(BuildContext context) => Stack(
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(_Constants.newsImageRadius),
-            child: Container(
-              color: Colors.black,
-              child: Opacity(
-                opacity: _Constants.newsImageLayerOpacity,
-                child: Image.network(
-                  news.imageUrl,
-                  //FIXME enter correct dimensions when filling a list with those items
-                  width: 300,
-                  height: 300,
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 1,
-            right: 1,
-            child: _buildBookmarkIcon(),
-          ),
-          _buildNewsInfo(context),
-        ],
-      );
-
-  Positioned _buildNewsInfo(BuildContext context) => Positioned(
-        bottom: 1,
-        left: 1,
-        child: Padding(
-          padding: const EdgeInsets.all(_Constants.stackItemPadding),
-          child: Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                _buildNewsTitle(context),
-                SizedBox(
-                  height: 15,
-                ),
-                _buildNewsAuthor(context),
-              ],
-            ),
-          ),
-        ),
-      );
-
-  Row _buildNewsAuthor(BuildContext context) => Row(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: _Constants.authorRightPadding),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(_Constants.authorImageRadius),
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(_Constants.imageRadius),
+          child: Container(
+            color: Colors.black,
+            child: Opacity(
+              opacity: _Constants.newsImageLayerOpacity,
               child: Image.network(
-                news.authorImageUrl,
+                news.imageUrl,
                 //FIXME enter correct dimensions when filling a list with those items
-                width: 40,
-                height: 40,
+                width: 300,
+                height: 300,
                 fit: BoxFit.fill,
               ),
             ),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          right: 0,
+          child: _buildBookmarkIcon(),
+        ),
+        Positioned(
+          left: 0,
+          bottom: 0,
+          child: _buildNewsInfo(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNewsInfo(BuildContext context) =>
+      Padding(
+        padding: const EdgeInsets.all(_Constants.stackItemPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildNewsTitle(context),
+            SizedBox(
+              height: 15,
+            ),
+            _buildNewsAuthor(context),
+          ],
+        ),
+      );
+
+  Widget _buildNewsAuthor(BuildContext context) =>
+      Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(_Constants.imageRadius),
+            child: Image.network(
+              news.authorImageUrl,
+              //FIXME enter correct dimensions when filling a list with those items
+              width: 40,
+              height: 40,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(
+            width: _Constants.authorDataSpacing,
           ),
           Text(
             news.authorName,
             style: Theme.of(context).textTheme.subtitle2.copyWith(
                   color: Colors.white,
-                  fontWeight: FontWeight.w300,
                 ),
           ),
         ],
       );
 
-  Container _buildNewsTitle(BuildContext context) => Container(
-        //FIXME enter correct dimensions when filling a list with those items
-        width: 270,
-        child: Column(
-          children: <Widget>[
-            Text(
-              news.title,
-              style: Theme.of(context).textTheme.subtitle1.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900,
-                  ),
-              maxLines: 3,
-            ),
-          ],
-        ),
-      );
+  Widget _buildNewsTitle(BuildContext context) {
+    var wrappingThreshold = MediaQuery
+        .of(context)
+        .size
+        .width * _Constants.wrappingThresholdPercent;
 
-  Padding _buildBookmarkIcon() => Padding(
+    return Container(
+      width: wrappingThreshold,
+      child: Column(
+        children: [
+          Text(
+            news.title,
+            style: Theme
+                .of(context)
+                .textTheme
+                .subtitle1
+                .copyWith(
+              color: Colors.white,
+            ),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 3,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBookmarkIcon() =>
+      Padding(
         padding: const EdgeInsets.all(_Constants.stackItemPadding),
         child: Icon(
           Icons.bookmark,
