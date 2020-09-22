eab1ba9e4472e77debaab9e65fd5a12a632caaaf
kightsonsanom <tomasz.olichwer@droidsonroids.pl>
2020-09-22 21:13:25 +0200

    NEWS-27 Simplify presentation steps provider

--- lib/src/ui/pages/presentation/presentation_steps_provider.dart
+++ lib/src/ui/pages/presentation/presentation_steps_provider.dart
@@ -1,25 +1,23 @@
 import 'package:allthenews/generated/l10n.dart';
 import 'package:allthenews/src/domain/presentation/presentation_step.dart';
-import 'package:allthenews/src/domain/presentation/presentation_steps_provider.dart';
 import 'package:allthenews/src/ui/common/util/untranslatable_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class PresentationStepsContextProvider implements PresentationStepsProvider<BuildContext> {
+class PresentationStepsProvider {
   List<PresentationStep> _presentationSteps = [];
 
-  @override
-  List<PresentationStep> provide(BuildContext buildContext) {
+  List<PresentationStep> provide() {
     if (_presentationSteps.isEmpty) {
       _presentationSteps = [
         PresentationStep(
           title: UntranslatableStrings.appName,
-          description: Strings.of(buildContext).appInfoTechnicalDescription,
+          description: Strings.current.appInfoTechnicalDescription,
           presentationImageType: PresentationImageType.flutterLogo,
         ),
         PresentationStep(
           title: UntranslatableStrings.appName,
-          description: Strings.of(buildContext).appInfoFeatureDescription,
+          description: Strings.current.appInfoFeatureDescription,
           presentationImageType: PresentationImageType.newsIcon,
         ),
       ];
