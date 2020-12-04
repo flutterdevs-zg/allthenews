25d2f04296501a872b388e80fa5e17f89539e4a6
Marek <maarekvogt@gmail.com>
2020-12-04 14:21:25 +0100

    [NEWS-33] added unit tests

--- test/api_stubs/api_stubs_reader.dart
+++ test/api_stubs/api_stubs_reader.dart
@@ -1,12 +1,3 @@
 import 'dart:io';
 
-String findApiStubBy(String name) => File('${_findTestDirectory()}/api_stubs/$name').readAsStringSync();
-
-//https://github.com/flutter/flutter/issues/20907
-String _findTestDirectory() {
-  var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
-  }
-  return dir;
-}
+String findApiStubBy(String name) => File('${Directory.current.path}/api_stubs/$name').readAsStringSync();
