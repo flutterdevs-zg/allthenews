0521c62661515bf05d228e9e38e416fed0758fd6
kightsonsanom <tomasz.olichwer@droidsonroids.pl>
2021-02-04 20:35:28 +0100

    NEWS-37 Obsluga kluczy map dla ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("add api key here")
+    GMSServices.provideAPIKey(credentials.mapsApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ ios/Runner/Credentials.generated.swift
@@ -0,0 +1,7 @@
+// Generated using Sourcery 1.0.2 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+public struct Credentials {
+	let mapsApiKey: String
+}
+public let credentials = Credentials(mapsApiKey: "add_api_key_here")
